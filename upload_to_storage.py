ac0035ea2b040287bba68be46329a7799b2a1879
Rahul-sigmoid <88548544+Rahul-sigmoid@users.noreply.github.com>
2021-08-16 20:05:11 +0530

    Update upload_to_storage.py

--- upload_to_storage.py
+++ upload_to_storage.py
@@ -1,29 +1,37 @@
 from google.cloud import storage
 
-# Setting credentials using the downloaded JSON file
+try:
+  
+    # Setting credentials using the downloaded JSON file
 
-client = storage.Client.from_service_account_json(json_credentials_path='/Users/rahul/Downloads/project-1-322814-d155a91c4d05.json')
+    client = storage.Client.from_service_account_json(json_credentials_path='/Users/rahul/Downloads/project-1-322814-d155a91c4d05.json')
 
-# create bucket
+    # create bucket
 
-bucket = client.create_bucket('upload_to_storage')
+    bucket = client.create_bucket('upload_to_storage')
 
-# get the bucket object
+    # get the bucket object
 
-bucket = client.get_bucket('upload_to_storage')
+    bucket = client.get_bucket('upload_to_storage')
 
-# Name of the object-1 to be stored in the bucket
+    # Name of the object-1 to be stored in the bucket
 
-object1 = bucket.blob('Orders.csv')
+    object1 = bucket.blob('Orders.csv')
 
-# Name of the object-1 in local file system
+    # Name of the object-1 in local file system
 
-object1.upload_from_filename('/Users/rahul/Downloads/Orders.csv')
+    object1.upload_from_filename('/Users/rahul/Downloads/Orders.csv')
 
-# Name of the object-2 to be stored in the bucket
+    # Name of the object-2 to be stored in the bucket
 
-object2 = bucket.blob('Customers.csv')
+    object2 = bucket.blob('Customers.csv')
 
-# Name of the object-2 in local file system
+    # Name of the object-2 in local file system
 
-object2.upload_from_filename('/Users/rahul/Downloads/Customers.csv')
+    object2.upload_from_filename('/Users/rahul/Downloads/Customers.csv')
+  
+except Exception as err:
+  
+    print(err)
+    
+    raise Exception("Table load failed for customers table")
